2dced1ee0e75dac202635791bd92c572c804f216
Abhinav Gupta <abhigupta1702@gmail.com>
2019-10-07 11:56:50 +0530

    Adding concurrency and retry

--- army-service/src/main/java/com/abhinav/armyservice/controller/ConsumerController.java
+++ army-service/src/main/java/com/abhinav/armyservice/controller/ConsumerController.java
@@ -1,5 +1,6 @@
 package com.abhinav.armyservice.controller;
 
+import lombok.SneakyThrows;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.cloud.stream.messaging.Sink;
@@ -10,7 +11,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class ConsumerController {
 
     @StreamListener(Sink.INPUT)
+    @SneakyThrows
     public void handleMessage(String msg) {
-        System.out.println("Received: " + msg);
+        System.out.println("Step1 - Received: " + msg);
+        if (msg.equals("Test1"))
+            throw new Exception("basnmdbsad");
+        Thread.sleep(20000);
+        System.out.println("Step2 - Received: " + msg);
     }
 }
